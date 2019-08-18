2832d034beaef94bf41399ebedde83cb8584f8d4
Pimthip Paiboonkasemsut <k.pimthip@gmail.com>
2019-08-18 17:23:49 +0700

    change line noti api

--- configs/line.js
+++ configs/line.js
@@ -1,4 +1,4 @@
 module.exports = {
-    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || '',
-    channelSecret: process.env.LINE_BOT_CHANNEL_SECRET || ''
+    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || '1610095569',
+    channelSecret: process.env.LINE_BOT_CHANNEL_SECRET || '969084aaf134818b9e144689604a0c6f'
 }
