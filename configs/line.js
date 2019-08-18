c8839d296a44cc0edf9bcd089fa19a0429b9d697
Pimthip Paiboonkasemsut <k.pimthip@gmail.com>
2019-08-18 17:38:40 +0700

    Change Line Access Token

--- configs/line.js
+++ configs/line.js
@@ -1,4 +1,4 @@
 module.exports = {
-    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || '1610095569',
+    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || 't3Mum5lxceanuijswAIIGibV3YmAh9sCFSw6ahutOVGOvzA5+wv6N8ZG7tu2PqkevY/0VL2zNE0QMBHBQcRCqbPEfA1+kIdzqEW18Tb5s4CrRvvOb++js7MXQW6ZaGL/pPHIt+VmjwH5pC2Eh5KIL49PbdgDzCFqoOLOYbqAITQ=',
     channelSecret: process.env.LINE_BOT_CHANNEL_SECRET || '969084aaf134818b9e144689604a0c6f'
 }
