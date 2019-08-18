2953bfa1d601ae454cae00263c5fa6e4dfd03ed0
Pimthip Paiboonkasemsut <k.pimthip@gmail.com>
2019-08-18 18:10:37 +0700

    Change Access token

--- configs/line.js
+++ configs/line.js
@@ -1,4 +1,4 @@
 module.exports = {
-    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || 't3Mum5lxceanuijswAIIGibV3YmAh9sCFSw6ahutOVGOvzA5+wv6N8ZG7tu2PqkevY/0VL2zNE0QMBHBQcRCqbPEfA1+kIdzqEW18Tb5s4CrRvvOb++js7MXQW6ZaGL/pPHIt+VmjwH5pC2Eh5KIL49PbdgDzCFqoOLOYbqAITQ=',
+    accessToken: process.env.LINE_BOT_CHANNEL_TOKEN || '2imSSWYU5Qvt0+dIWdWendYZ2aNNeZZY2wmDBq55C2YodzDRAksVx7wbzkjsAhbLvY/0VL2zNE0QMBHBQcRCqbPEfA1+kIdzqEW18Tb5s4D2yXMs0QYskTUnLmx9q8NL3cyxnUm3/uykqYyEluqFygdB04t89/1O/w1cDnyilFU=',
     channelSecret: process.env.LINE_BOT_CHANNEL_SECRET || '969084aaf134818b9e144689604a0c6f'
 }
