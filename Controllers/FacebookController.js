a304a41b4aa66fd9d55b455e2b7fd205bd461b92
Pimthip Paiboonkasemsut <k.pimthip@gmail.com>
2019-09-15 16:32:52 +0700

    add Facebook support

--- Controllers/FacebookController.js
+++ Controllers/FacebookController.js
@@ -2,18 +2,53 @@ const config = require('../configs/facebook')
 
 module.exports = {
     webhook : async (req, res) =>{   
-        let VERIFY_TOKEN = 'pusher-bot';
-
+        let VERIFY_TOKEN = "EAADKG4ZBPi64BAHcIMgDJt1ILIiusTyAUp1D5dGrTjoH219KwbEDxjoZA54JBZADvoKeXV0ybu55D1a1rVm98dEVCyIn460KWsZC89cLk2NtBmjeLMEcK7P5s7ZANFSGMUIxykAdVcMCGJqJnaDexbcJSB2uQrxYtM254OSr88FhE3f8SxvfJ"
+    
+        // Parse the query params
         let mode = req.query['hub.mode'];
         let token = req.query['hub.verify_token'];
         let challenge = req.query['hub.challenge'];
-
-        if (mode && token === VERIFY_TOKEN) {
+            
+        // Checks if a token and mode is in the query string of the request
+        if (mode && token) {
+        
+            // Checks the mode and token sent is correct
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
+            
+            } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);      
+            }
         }
     },
+    posthook: async (req, res) => {  
+ 
+        let body = req.body;
+      
+        // Checks this is an event from a page subscription
+        if (body.object === 'page') {
+      
+          // Iterates over each entry - there may be multiple if batched
+          body.entry.forEach(function(entry) {
+      
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+          });
+      
+          // Returns a '200 OK' response to all requests
+          res.status(200).send('EVENT_RECEIVED');
+        } else {
+          // Returns a '404 Not Found' if event is not from a page subscription
+          res.sendStatus(404);
+        }
+      
+    }
     
 
 }
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 const routes = require('express').Router();
 const EmailController = require('./Controllers/EmailController')
 const LineController = require('./Controllers/LineController')
-const FBController = require('./Controllers/LineController')
+const FBController = require('./Controllers/FacebookController')
 
 routes.post('/email', EmailController.send);
 routes.post('/line/webhook', LineController.signature, LineController.webhook);
 routes.get('/fb/webhook', FBController.webhook);
+routes.post('/fb/webhook', FBController.posthook);
 
 module.exports = routes;
